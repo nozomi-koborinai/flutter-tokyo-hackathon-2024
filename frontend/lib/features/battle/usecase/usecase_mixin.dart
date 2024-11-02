be5a56059c5cd2c344f1e002edf2670ca74d258c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-11-02 15:29:31 +0900

    refactor: userRepository の移動

---
+++ frontend/lib/features/battle/usecase/usecase_mixin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tokyo_hackathon_2024/core/widgets/loading.dart';
+
+/// ユースケース実行のためのメソッドを備えた Mixin
+mixin UsecaseMixin {
+  Future<T> run<T>(
+    Ref ref, {
+    required Future<T> Function() action,
+  }) async {
+    ref.read(overlayLoadingProvider.notifier).update((_) => true);
+    try {
+      return await action();
+    } catch (e) {
+      rethrow;
+    } finally {
+      ref.read(overlayLoadingProvider.notifier).update((_) => false);
+    }
+  }
+}
