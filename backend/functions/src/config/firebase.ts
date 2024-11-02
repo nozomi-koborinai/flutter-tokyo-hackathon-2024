59c222356de2fac67507d41c4b6b9a5dd6d87eba
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-11-02 13:05:35 +0900

    fix: lint

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -1,29 +1,35 @@
-import { configureGenkit } from '@genkit-ai/core'
-import { dotprompt } from '@genkit-ai/dotprompt'
-import { firebase } from '@genkit-ai/firebase'
-import { googleCloud } from '@genkit-ai/google-cloud'
-import { googleAI } from '@genkit-ai/googleai'
-import { vertexAI } from '@genkit-ai/vertexai'
-import * as admin from 'firebase-admin'
-import { defineSecret } from 'firebase-functions/params'
-import * as z from 'zod'
+import { configureGenkit } from "@genkit-ai/core";
+import { dotprompt } from "@genkit-ai/dotprompt";
+import { firebase } from "@genkit-ai/firebase";
+import { googleCloud } from "@genkit-ai/google-cloud";
+import { googleAI } from "@genkit-ai/googleai";
+import { vertexAI } from "@genkit-ai/vertexai";
+import * as admin from "firebase-admin";
+import { defineSecret } from "firebase-functions/params";
+import * as z from "zod";
 
 configureGenkit({
-  plugins: [firebase(), vertexAI(), googleAI({ apiVersion: `v1beta` }), dotprompt(), googleCloud()],
+  plugins: [
+    firebase(),
+    vertexAI(),
+    googleAI({ apiVersion: `v1beta` }),
+    dotprompt(),
+    googleCloud(),
+  ],
   logLevel: `debug`,
   enableTracingAndMetrics: true,
   telemetry: {
     instrumentation: `googleCloud`,
     logger: `googleCloud`,
   },
-})
+});
 
-admin.initializeApp()
+admin.initializeApp();
 
 export const getPunishmentSchema = z.object({
   existingPunishments: z.array(z.string()),
-})
+});
 
-export const db = admin.firestore()
+export const db = admin.firestore();
 
-export const googleAIapiKey = defineSecret(`GOOGLE_GENAI_API_KEY`)
+export const googleAIapiKey = defineSecret(`GOOGLE_GENAI_API_KEY`);
